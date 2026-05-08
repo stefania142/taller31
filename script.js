4ce2c94df2d92f22a6c42fdb172f49e1a9a33f90
stefania <est.stefania.trianar@unimilitar.edu.co>
2026-05-08 12:28:42 -0500

    Implementacion inicial de escenas y viewport

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 console.log(canvas);
+
+const escenas = [
+
+    {x1:150, y1:150, x2:350, y2:250},
+    {x1:20, y1:20, x2:80, y2:60},
+    {x1:50, y1:200, x2:250, y2:220}
+
+];
+
+let escenaActual = 0;
+
+function convertirY(y){
+    return y;
+}
+
+
+function plot(x, y){
+
+    ctx.fillRect(x, convertirY(y), 2, 2);
+}
+
 // dibujar pixel
 function plot(x, y){
 
@@ -32,6 +53,18 @@ function dibujarLinea(x1, y1, x2, y2){
         y += yinc;
     }
 }
+function dibujarViewport(){
 
-// prueba
-dibujarLinea(100, 100, 400, 300);
+    dibujarLinea(100,100,400,100);
+    dibujarLinea(400,100,400,300);
+    dibujarLinea(400,300,100,300);
+    dibujarLinea(100,300,100,100);
+}
+
+
+dibujarViewport();
+
+const linea = escenas[escenaActual];
+
+dibujarLinea(linea.x1, linea.y1, linea.x2, linea.y2);
+
