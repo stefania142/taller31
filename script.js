b35a3756eef1155b5dc4b346e41e4ee4dfa77f1c
stefania <est.stefania.trianar@unimilitar.edu.co>
2026-05-08 13:10:36 -0500

    Integracion inicial del algoritmo Cohen Sutherland y añadi mas escenas parecidas al grafico

--- script.js
+++ script.js
@@ -10,9 +10,17 @@ const escenas = [
     {x1:20, y1:20, x2:80, y2:60},
     {x1:50, y1:200, x2:250, y2:220},
     {x1:200, y1:50, x2:250, y2:250},
-    {x1:50, y1:50, x2:500, y2:350}
+    {x1:50, y1:50, x2:500, y2:350},
+    {x1:50, y1:50, x2:100, y2:100},
+    {x1:250, y1:50, x2:250, y2:400},
 
 ];
+
+const INSIDE = 0;
+const LEFT = 1;
+const RIGHT = 2;
+const BOTTOM = 4;
+const TOP = 8;
 
 let escenaActual = 0;
 
@@ -67,9 +75,37 @@ function dibujarViewport(){
     dibujarLinea(400,300,100,300);
     dibujarLinea(100,300,100,100);
 }
-
-
 dibujarViewport();
+
+function obtenerCodigo(x, y){
+
+    let codigo = INSIDE;
+
+    if(x < 100){
+        codigo |= LEFT;
+    }
+
+    else if(x > 400){
+        codigo |= RIGHT;
+    }
+
+    if(y < 100){
+        codigo |= BOTTOM;
+    }
+
+    else if(y > 300){
+        codigo |= TOP;
+    }
+
+    return codigo;
+}
+function cohenSutherland(x1, y1, x2, y2){
+
+    let codigo1 = obtenerCodigo(x1,y1);
+    let codigo2 = obtenerCodigo(x2,y2);
+
+    console.log(codigo1, codigo2);
+}
 
 const linea = escenas[escenaActual];
 
@@ -110,6 +146,12 @@ function anteriorEscena(){
     renderizar();
 }
 
-
 renderizar();
 
+dibujarLinea(linea.x1, linea.y1, linea.x2, linea.y2);
+cohenSutherland(
+    linea.x1,
+    linea.y1,
+    linea.x2,
+    linea.y2
+);
